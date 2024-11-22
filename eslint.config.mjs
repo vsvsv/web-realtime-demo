adb7de9c2ee11d7c2417b031aad128db45c3a2ac
vsvsv <vsevolodplatunov@gmail.com>
2024-11-23 01:37:38 +0300

    Basic project structure

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import elsintJsPlugin from '@eslint/js';
+import typescriptEslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config} */
+const customConfig = {
+    rules: {
+        'prefer-const': 'error',
+        'no-debugger': 'error',
+        'no-console': 'error',
+        'prefer-arrow-callback': 'error',
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+};
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.browser } },
+    elsintJsPlugin.configs.recommended,
+    ...typescriptEslint.configs.recommended,
+    customConfig,
+];
