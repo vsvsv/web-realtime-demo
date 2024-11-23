786b84ba2c8665e9666fba1a54965b052844bc0a
vsvsv <vsevolodplatunov@gmail.com>
2024-11-23 03:18:21 +0300

    Add SCSS support

---
+++ client/src/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header = () => (
+    <h1 className="sample-style sample-style-nested">
+        Realtime Web API bench
+    </h1>
+);
---
+++ client/src/main.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Header } from './Header';
+import '../styles/globals.scss';
+
+const setupReactRoot = (rootComponent: React.FC, rootElementId: string) => {
+    const rootDiv = document.getElementById(rootElementId);
+    if (rootDiv) {
+        const reactRoot = ReactDOM.createRoot(rootDiv);
+        reactRoot.render(React.createElement(rootComponent, {}, null));
+    }
+};
+
+setupReactRoot(Header, 'root');
