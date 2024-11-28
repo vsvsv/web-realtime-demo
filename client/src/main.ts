50ecd12830e31943cee1cee514ab6218b11e1c4d
vsvsv <vsevolodplatunov@gmail.com>
2024-11-28 05:20:42 +0300

    Bring back react strict mode

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,12 +8,11 @@ const setupReactRoot = (rootComponent: React.FC, rootElementId: string) => {
     const rootDiv = document.getElementById(rootElementId);
     if (rootDiv) {
         const reactRoot = ReactDOM.createRoot(rootDiv);
-        // const reactElem = React.createElement(
-        //     React.StrictMode,
-        //     {},
-        //     React.createElement(rootComponent, {}, null),
-        // );
-        const reactElem = React.createElement(rootComponent, {}, null);
+        const reactElem = React.createElement(
+            React.StrictMode,
+            {},
+            React.createElement(rootComponent, {}, null),
+        );
         reactRoot.render(reactElem);
     }
 };
